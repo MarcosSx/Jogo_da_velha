ce232b35afe8ab34c31a587c9cb64bed632f4c06
Marcos <marcosxavier03@gmail.com>
2022-01-06 17:11:51 -0300

    Jogo finalizado

--- Jogo.py
+++ Jogo.py
@@ -8,23 +8,21 @@ while True:
     jogador2 = escolhe_jogador('JOGADOR 2: ', jogador1)
     cabeçalho('Escolha um dos indices numerados')
     atualiza_jogo(jogo)
-    while True:
-        for i in range(0, 9):
-            if i % 2 == 0:
-                pos = int(input('Digite uma posição: '))
-                preencher_posicao(pos, jogo, jogador1)
-                atualiza_jogo(jogo)
-                if jogador_ganhou(jogo, jogador1):
-                    cabeçalho(f'\033[32mO Jogador 1 {jogador1} GANHOUU!!!\033[m')
-                    break
-            else:
-                pos = int(input('Digite uma posição: '))
-                preencher_posicao(pos, jogo, jogador2)
-                atualiza_jogo(jogo)
-                if jogador_ganhou(jogo, jogador2):
-                    cabeçalho(f'\033[32mO Jogador 2 {jogador2} GANHOUU!!!\033[m')
-                    break
-        break
+    for i in range(0, 9):
+        if i % 2 == 0:
+            pos = leiaInt('Digite uma posição: ')
+            preencher_posicao(pos, jogo, jogador1)
+            atualiza_jogo(jogo)
+            if jogador_ganhou(jogo, jogador1):
+                cabeçalho(f'\033[32mO Jogador 1 {jogador1} GANHOUU!!!\033[m')
+                break
+        else:
+            pos = leiaInt('Digite uma posição: ')
+            preencher_posicao(pos, jogo, jogador2)
+            atualiza_jogo(jogo)
+            if jogador_ganhou(jogo, jogador2):
+                cabeçalho(f'\033[32mO Jogador 2 {jogador2} GANHOUU!!!\033[m')
+                break
     while True:
         resp = str(input('Deseja jogar mais uma? [S/N]: ')).upper().strip()[0]
         if resp in 'SN':
