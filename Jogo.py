82a3c9cb52d3b82c5b7643070748fe30748d5ce3
Marcos <marcosxavier03@gmail.com>
2022-01-10 22:38:29 -0300

    Melhorias

--- Jogo.py
+++ Jogo.py
@@ -3,12 +3,15 @@ from lib import *
 while True:
     jogo = cria_novo_jogo(inicio=True)
     jogador1 = escolhe_jogador('JOGADOR 1: ')
-    escolha = leiaInt('Deseja jogar contra:\n1: Pessoa\n2: BOT\n>>')
+    while True:
+        escolha = leiaInt('Deseja jogar contra:\n1: Pessoa\n2: BOT\n>> Opção [1/2]: ')
+        if str(escolha) in '12':
+            break
+        cabeçalho('Opção invalida!', 2)
     bot = False
     if escolha == 2:
         bot = True
     jogador2 = escolhe_jogador('JOGADOR 2: ', jogador1, bot)
-
     atualiza_jogo(jogo)
     cont = 0
     for i in range(0, 9):
@@ -29,7 +32,7 @@ while True:
             if jogador_ganhou(jogo, jogador2):
                 cabeçalho(f'O Jogador 2 {jogador2} GANHOUU!!!', cor=3)
                 break
-    if cont == 9 and not jogador_ganhou(jogo, jogador1) or not jogador_ganhou(jogo, jogador2):
+    if cont == 9 and not jogador_ganhou(jogo, jogador1) and not jogador_ganhou(jogo, jogador2):
         cabeçalho('DEU VELHA', cor=9)
     while True:
         resp = str(input('Deseja jogar mais uma? [S/N]: ')).upper().strip()[0]
--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,9 @@
 from random import randint
+from time import sleep
 
 c = (
     '\033[m',       # 0Limpa
-    '\033[0;30m',   # 1Preto
+    '\033[1;97m',   # 1Branco
     '\033[0;31m',   # 2Vermelho
     '\033[0;32m',   # 3Verde
     '\033[0;33m',   # 4Amarelo
@@ -16,6 +17,11 @@ c = (
 
 
 def cria_novo_jogo(inicio=False):
+    '''
+    -> Esta função cria uma lista onde o jogo é armazenado.
+    :param inicio: Se for verdadeiro imprime os cabeçalhos de um jogo novo
+    :return: Retorna lista do jogo independente de como esteja o jogo
+    '''
     if inicio:
         cabeçalho('BEM VINDO À VELHA', cor=5)
         cabeçalho('Escolha um caractere para te representar:', cor=8)
@@ -23,6 +29,13 @@ def cria_novo_jogo(inicio=False):
 
 
 def escolhe_jogador(msg, jogador2='x', bot=False):
+    '''
+    -> Esta função escolhe os caracteres que cada jogador devera usar
+    :param msg: Este parametro imprime na tela a mensagem que o jogador deseja
+    :param jogador2: Este parametro (opcional), define qual caractere esta disponivel
+    :param bot: Se verdadeiro, escolhe sozinho seu caractere, se falso, o segundo jogador devera escolher seu caractere
+    :return: Restorna o valor do caractere do jogador
+    '''
     if bot:
         jogador = 'X'
         if jogador in jogador2:
@@ -38,8 +51,12 @@ def escolhe_jogador(msg, jogador2='x', bot=False):
     return c[cor] + jogador + c[0]
 
 
-
 def atualiza_jogo(game):
+    '''
+    -> Esta função imprime o jogo no estado em que está
+    :param game: Lista do jogo
+    :return: Não retorna nada apenas imprime o jogo
+    '''
     cabeçalho('Escolha um dos indices numerados', cor=8)
     for lin in range(0, 3):
         for col in range(0, 3):
@@ -49,38 +66,53 @@ def atualiza_jogo(game):
         print(f'{c[8]}-' * 11 if lin != 2 else f'{c[0]}')
 
 
-def preencher_posicao(pos, game, jogador, bot=False):
-    while pos > 9 or pos <= 0:
+def preencher_posicao(posição, game, jogador, bot=False):
+    '''
+    -> Esta função preenche apenas espaços vazios
+    :param posição: Posição escolhida pelo jogador
+    :param game: Lista do jogo
+    :param jogador: Caractere do jogador
+    :param bot: Caractere do jogador("Computador")
+    :return: -
+    '''
+    while posição > 9 or posição <= 0:
         if bot:
-            pos = randint(1, 9)
+            posição = randint(1, 9)
+            sleep(1)
             break
-        cabeçalho(f'Indice {pos} fora de alcance')
-        pos = leiaInt('Digite uma posição valida: ', 4)
+        cabeçalho(f'Indice {posição} fora de alcance')
+        posição = leiaInt('Digite uma posição valida: ', 4)
         print()
     ok = False
     cont = 0
-    cabeçalho(f'{c[4]}O jogador {c[0]}{jogador}{c[4]} jogou na posição {str(pos) + c[0]}')
+    cabeçalho(f'{c[4]}O jogador {c[0]}{jogador}{c[4]} jogou na posição {str(posição) + c[0]}')
     while True:
         if ok:
             break
         for l in range(0, 3):
             try:
-                col = game[l].index(str(pos))
+                col = game[l].index(str(posição))
                 game[l][col] = jogador
                 ok = True
             except:
                 cont += 1
                 if cont == 3:
                     if bot:
-                        pos = randint(1, 9)
+                        posição = randint(1, 9)
                     else:
-                        pos = leiaInt('Posição ja preencida\nEscolha outra posição: ', 2)
-                        cabeçalho(f'{c[4]}O jogador {c[0]}{jogador}{c[4]} jogou na posição {pos}{c[0]}')
+                        posição = leiaInt('Posição ja preencida\nEscolha outra posição: ', 2)
+                        cabeçalho(f'{c[4]}O jogador {c[0]}{jogador}{c[4]} jogou na posição {posição}{c[0]}')
                     cont = 0
                     break
 
 
 def jogador_ganhou(game, jogador):
+    '''
+    -> Esta função verifica todas as diagonais, verticais e horizontais e diz se o jogador ganhoy
+    :param game: Lista do jogo
+    :param jogador: Jogador
+    :return: Retorna True se o jogador ganhou
+    '''
     # Verifica se a horizontal está igual
     for l in range(0, 3):
         cont_h = 0
@@ -119,17 +151,26 @@ def jogador_ganhou(game, jogador):
                 return True
 
 
-def linha(tam=42):
-   return '-' * tam
-
-
 def cabeçalho(txt, cor=0):
-    print(c[7] + linha() + c[0])
+    '''
+    -> Esta função faz os cabeçalhos do jogo
+    :param txt: Texto a ser impresso no cabeçaçho
+    :param cor: Cor escolhida
+    :return: Imprime o cabeçalho
+    '''
+    linha = '-' * 42
+    print(c[7] + linha + c[0])
     print(c[cor] + txt.center(42) + c[0])
-    print(c[7] + linha() + c[0])
+    print(c[7] + linha + c[0])
 
 
 def leiaInt(msg, cor=8):
+    '''
+    -> Função para ler apenas numeros inteiros
+    :param msg: Texto para input
+    :param cor: Cor escolhida
+    :return: Retorna o valor inteiro
+    '''
     ok = False
     valor = 0
     while True:
